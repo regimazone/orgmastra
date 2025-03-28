a48e26160a437db46742780bb0b22fcc9136d949
vishesh <visheshbaghel99@gmail.com>
2025-03-28 23:29:54 +0530

    added support for saving flattened nested coloumns in table

--- stores/lance/src/vector/index.test.ts
+++ stores/lance/src/vector/index.test.ts
@@ -228,6 +228,62 @@ describe('Lance vector store tests', () => {
     });
   });
 
+  describe('Create table operations', () => {
+    const testTableName = 'test-table' + Date.now();
+
+    beforeAll(async () => {
+      vectorDB.deleteAllTables();
+    });
+
+    it('should throw error when no data is provided', async () => {
+      await expect(vectorDB.createTable(testTableName, [])).rejects.toThrow(
+        'Failed to create table: At least one record or a schema needs to be provided',
+      );
+    });
+
+    it('should create a new table', async () => {
+      await vectorDB.createTable(testTableName, [{ id: '1', vector: [0.1, 0.2, 0.3] }]);
+
+      const tables = await vectorDB.listTables();
+      expect(tables).toContain(testTableName);
+
+      const schema = await vectorDB.getTableSchema(testTableName);
+      expect(schema.fields.map(field => field.name)).toEqual(['id', 'vector']);
+    });
+
+    it('should throw error when creating existing table', async () => {
+      const tableName = 'test-table' + Date.now();
+      await vectorDB.createTable(tableName, [{ id: '1', vector: [0.1, 0.2, 0.3] }]);
+
+      await expect(vectorDB.createTable(tableName, [{ id: '1', vector: [0.1, 0.2, 0.3] }])).rejects.toThrow(
+        'already exists',
+      );
+    });
+
+    it('should create a table with single level nested metadata object by flattening it', async () => {
+      const tableName = 'test-table' + Date.now();
+      await vectorDB.createTable(tableName, [{ id: '1', vector: [0.1, 0.2, 0.3], metadata: { name: 'test' } }]);
+
+      const schema = await vectorDB.getTableSchema(tableName);
+      expect(schema.fields.map((field: any) => field.name)).toEqual(['id', 'vector', 'metadata_name']);
+    });
+
+    it('should create a table with multi level nested metadata object by flattening it', async () => {
+      const tableName = 'test-table' + Date.now();
+      await vectorDB.createTable(tableName, [
+        { id: '1', vector: [0.1, 0.2, 0.3], metadata: { name: 'test', details: { text: 'test' } } },
+      ]);
+
+      const schema = await vectorDB.getTableSchema(tableName);
+      expect(schema.fields.map((field: any) => field.name)).toEqual([
+        'id',
+        'vector',
+        'metadata_name',
+        'metadata_details_text',
+      ]);
+    });
+  });
+
   describe('Vector operations', () => {
     describe('upsert operations', () => {
       const testTableName = 'test-table' + Date.now();
@@ -692,6 +748,68 @@ describe('Lance vector store tests', () => {
         topK: 3,
         includeVector: true,
         filter: { text: 'First vector' },
+      });
+
+      expect(res).toHaveLength(1);
+      expect(res[0].id).toBe(ids[0]);
+      expect(res[0].metadata?.text).to.equal('First vector');
+      expect(res[0].metadata?.newText).to.equal('hi');
+    });
+  });
+
+  describe('Advanced query operations', () => {
+    const testTableName = 'test-table-advanced' + Date.now();
+    const testTableIndexColumn = 'vector';
+
+    beforeAll(async () => {
+      vectorDB.deleteAllTables();
+
+      const generateTableData = (numRows: number) => {
+        return Array.from({ length: numRows }, (_, i) => ({
+          id: String(i + 1),
+          vector: Array.from({ length: 3 }, () => Math.random()),
+          metadata: { name: 'test', details: { text: 'test' } },
+        }));
+      };
+
+      await vectorDB.createTable(testTableName, generateTableData(300));
+
+      await vectorDB.createIndex({
+        indexConfig: {
+          type: 'ivfflat',
+          numPartitions: 1,
+          numSubVectors: 1,
+        },
+        indexName: testTableIndexColumn,
+        dimension: 3,
+        tableName: testTableName,
+      });
+    });
+
+    afterAll(async () => {
+      vectorDB.deleteTable(testTableName);
+    });
+
+    it('should query vectors with nested metadata filter', async () => {
+      const testVectors = [[0.1, 0.2, 0.3]];
+      const ids = await vectorDB.upsert({
+        indexName: testTableIndexColumn,
+        tableName: testTableName,
+        vectors: testVectors,
+        metadata: [{ name: 'test2', details: { text: 'test2' } }],
+      });
+
+      expect(ids).toHaveLength(1);
+      expect(ids.every(id => typeof id === 'string')).toBe(true);
+
+      const res = await vectorDB.query({
+        indexName: testTableIndexColumn,
+        tableName: testTableName,
+        queryVector: testVectors[0],
+        columns: ['id', 'text', 'metadata_name', 'metadata_details_text', 'vector'],
+        topK: 3,
+        includeVector: true,
+        filter: { name: 'test2' },
       });
 
       expect(res).toHaveLength(1);
