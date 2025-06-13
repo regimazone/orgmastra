9ffc710c10f8815324c07aeb788ef7c3a5bc6ce6
mfrachet <marvin@mastra.ai>
2025-06-13 11:29:54 +0200

    SAVEPOINT

--- e2e-tests/kitchen-sink/prepare.js
+++ e2e-tests/kitchen-sink/prepare.js
@@ -19,6 +19,7 @@ export async function setupTestProject(pathToStoreFiles) {
     shell: true,
   });
 
+  console.log('Building project...');
   spawnSync('pnpm', ['build'], {
     cwd: newPath,
     stdio: 'inherit',
