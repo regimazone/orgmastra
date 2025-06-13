44ef27706b24f787f7c21912598b1cd58563e9fd
mfrachet <marvin@mastra.ai>
2025-06-13 12:21:24 +0200

    SAVEPOINT

--- e2e-tests/kitchen-sink/prepare.js
+++ e2e-tests/kitchen-sink/prepare.js
@@ -1,7 +1,6 @@
-import { spawn, spawnSync } from 'node:child_process';
+import { spawnSync } from 'node:child_process';
 import { cp, mkdir } from 'node:fs/promises';
 import { dirname, join } from 'node:path';
-import { stdout } from 'node:process';
 import { fileURLToPath } from 'node:url';
 
 export async function setupTestProject(pathToStoreFiles) {
@@ -12,8 +11,6 @@ export async function setupTestProject(pathToStoreFiles) {
 
   await mkdir(newPath, { recursive: true });
   await cp(projectPath, newPath, { recursive: true });
-
-  process.stdout.write('Global setup completed successfully\n');
 
   spawnSync('pnpm', ['install'], {
     cwd: newPath,
@@ -28,28 +25,31 @@ export async function setupTestProject(pathToStoreFiles) {
     shell: true,
   });
 
-  process.stdout.write('Global setup completed successfully\n');
-
   await pingMastraServer();
 }
 
 const pingMastraServer = async () => {
   let intervalCount = 0;
-  const intervalId = setInterval(async () => {
-    if (intervalCount > 10) {
-      clearInterval(intervalId);
-    }
 
-    fetch('http://localhost:4111/agents')
-      .then(res => {
-        if (res.ok) {
-          clearInterval(intervalId);
-        }
-      })
-      .catch(() => {
-        console.log(`Server sleeping... Attempt: ${intervalCount}`);
-      });
+  return new Promise((resolve, reject) => {
+    const intervalId = setInterval(async () => {
+      if (intervalCount > 10) {
+        clearInterval(intervalId);
+        reject(new Error('Mastra server not responding'));
+      }
 
-    intervalCount++;
-  }, 100);
+      fetch('http://localhost:4111/agents')
+        .then(res => {
+          if (res.ok) {
+            clearInterval(intervalId);
+            resolve();
+          }
+        })
+        .catch(() => {
+          console.log(`Server sleeping... Attempt: ${intervalCount}`);
+        });
+
+      intervalCount++;
+    }, 100);
+  });
 };
--- packages/memory/integration-tests/CHANGELOG.md
+++ packages/memory/integration-tests/CHANGELOG.md
@@ -1,5 +1,7 @@
 # @mastra/memory-integration-tests
 
+## 0.0.0-create-mastra-e2e-test-20250613101705
+
 ## 0.0.0-create-mastra-e2e-test-20250613093653
 
 ## 0.0.0-create-mastra-e2e-test-20250613093500
