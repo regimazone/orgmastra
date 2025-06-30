d41517dde1795979092f54d90196d6b90e41c597
Yujohn Nattrass <Yujohn.natt@gmail.com>
2025-06-30 12:49:23 -0700

    register eval hooks from remove from deployers

--- deployers/cloudflare/src/index.ts
+++ deployers/cloudflare/src/index.ts
@@ -109,48 +109,6 @@ export class CloudflareDeployer extends Deployer {
     import '#polyfills';
     import { mastra } from '#mastra';
     import { createHonoServer } from '#server';
-    import { evaluate } from '@mastra/core/eval';
-    import { AvailableHooks, registerHook } from '@mastra/core/hooks';
-    import { TABLE_EVALS } from '@mastra/core/storage';
-    import { checkEvalStorageFields } from '@mastra/core/utils';
-
-    registerHook(AvailableHooks.ON_GENERATION, ({ input, output, metric, runId, agentName, instructions }) => {
-      evaluate({
-        agentName,
-        input,
-        metric,
-        output,
-        runId,
-        globalRunId: runId,
-        instructions,
-      });
-    });
-
-    registerHook(AvailableHooks.ON_EVALUATION, async traceObject => {
-      const storage = mastra.getStorage();
-      if (storage) {
-        // Check for required fields
-        const logger = mastra?.getLogger();
-        const areFieldsValid = checkEvalStorageFields(traceObject, logger);
-        if (!areFieldsValid) return;
-
-        await storage.insert({
-          tableName: TABLE_EVALS,
-          record: {
-            input: traceObject.input,
-            output: traceObject.output,
-            result: JSON.stringify(traceObject.result || {}),
-            agent_name: traceObject.agentName,
-            metric_name: traceObject.metricName,
-            instructions: traceObject.instructions,
-            test_info: null,
-            global_run_id: traceObject.globalRunId,
-            run_id: traceObject.runId,
-            created_at: new Date().toISOString(),
-          },
-        });
-      }
-    });
 
     export default {
       fetch: async (request, env, context) => {
