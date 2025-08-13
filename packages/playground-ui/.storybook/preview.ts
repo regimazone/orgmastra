afb50031b397e27664c9ed929ac1215ec105d153
Greg Lobinski <greglobinski@gmail.com>
2025-08-13 12:39:25 +0200

    set dark theme

--- packages/playground-ui/.storybook/preview.ts
+++ packages/playground-ui/.storybook/preview.ts
@@ -1,14 +1,22 @@
 import type { Preview } from '@storybook/react-vite';
+import { themes } from 'storybook/theming';
 import '../src/index.css'; // Import Tailwind CSS
 
 const preview: Preview = {
   parameters: {
+    docs: {
+      theme: themes.dark,
+    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/i,
       },
     },
+  },
+  initialGlobals: {
+    // 👇 Set the initial background color
+    backgrounds: { value: 'dark' },
   },
 };
 
