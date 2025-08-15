02c4145ef449e452f5a3549dbdc3a62f9ac91e16
Tyler Barnes <tylerdbarnes@gmail.com>
2025-08-15 14:48:05 -0700

    remove trace

--- packages/core/src/agent/save-queue/index.ts
+++ packages/core/src/agent/save-queue/index.ts
@@ -125,7 +125,6 @@ export class SaveQueueManager {
   async flushMessages(messageList: MessageList, threadId?: string, memoryConfig?: MemoryConfig) {
     if (!threadId) return;
     this.clearDebounce(threadId);
-    console.trace('persistUnsavedMessages');
     return this.enqueueSave(threadId, messageList, memoryConfig);
   }
 }
