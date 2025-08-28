f8b63bcdeb3f6626ec2b3c4e81130c70f973580f
mfrachet <marvin@mastra.ai>
2025-08-28 14:05:24 +0200

    fix button

--- packages/playground-ui/src/components/ui/elements/buttons/button.tsx
+++ packages/playground-ui/src/components/ui/elements/buttons/button.tsx
@@ -16,23 +16,24 @@ export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, as, variant = 'outline', ...props }: ButtonProps, ref) => {
     const Component = as || 'button';
 
-  return (
-    <Component
-      className={cn(
-        'text-[.875rem] inline-flex items-center justify-center rounded-lg px-[1rem] gap-[.75rem] leading-0 border bg-transparent text-[rgba(255,255,255,0.7)]',
-        '[&:not(:disabled):hover]:border-[rgba(255,255,255,0.25)] [&:not(:disabled):hover]:text-[rgba(255,255,255,0.9)]',
-        '[&>svg]:w-[1em] [&>svg]:h-[1em] [&>svg]:mx-[-0.3em] [&>svg]:opacity-70',
-        className,
-        {
-          'cursor-not-allowed opacity-50': props.disabled,
-          'bg-ui-primaryBtnBg text-ui-primaryBtnText hover:bg-surface6 leading-[0] font-semibold':
-            variant === 'primary',
-          'min-h-[2rem]': variant === 'ghost',
-          'min-h-[2.5rem]': variant !== 'ghost',
-          'border-[rgba(255,255,255,0.15)]': variant === 'outline',
-        },
-      )}
-      {...props}
-    />
-  );
-};
+    return (
+      <Component
+        className={cn(
+          'text-[.875rem] inline-flex items-center justify-center rounded-lg px-[1rem] gap-[.75rem] leading-0 border bg-transparent text-[rgba(255,255,255,0.7)]',
+          '[&:not(:disabled):hover]:border-[rgba(255,255,255,0.25)] [&:not(:disabled):hover]:text-[rgba(255,255,255,0.9)]',
+          '[&>svg]:w-[1em] [&>svg]:h-[1em] [&>svg]:mx-[-0.3em] [&>svg]:opacity-70',
+          className,
+          {
+            'cursor-not-allowed opacity-50': props.disabled,
+            'bg-ui-primaryBtnBg text-ui-primaryBtnText hover:bg-surface6 leading-[0] font-semibold':
+              variant === 'primary',
+            'min-h-[2rem]': variant === 'ghost',
+            'min-h-[2.5rem]': variant !== 'ghost',
+            'border-[rgba(255,255,255,0.15)]': variant === 'outline',
+          },
+        )}
+        {...props}
+      />
+    );
+  },
+);
