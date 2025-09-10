f784963a7b8e3b6d0275b5ef0ff3edbf44fdd2a8
mfrachet <marvin@mastra.ai>
2025-09-10 15:32:24 +0200

    first network ui

--- packages/playground-ui/src/services/handle-network-message-from-memory.ts
+++ packages/playground-ui/src/services/handle-network-message-from-memory.ts
@@ -1,3 +1,4 @@
+import { BadgeMessage } from '@/components/assistant-ui/tools/badges/agent-badge';
 import { ThreadMessageLike } from '@assistant-ui/react';
 
 export const handleNetworkMessageFromMemory = (content: any): ThreadMessageLike => {
@@ -17,6 +18,25 @@ export const handleNetworkMessageFromMemory = (content: any): ThreadMessageLike 
 
   if (content.resourceType === 'agent') {
     console.log('content', content);
+    const badgeMessages: BadgeMessage[] = [];
+
+    return {
+      role: 'assistant',
+      content: [
+        {
+          type: 'tool-call',
+          toolCallId: content.finalResult.runId,
+          toolName: content.resourceId,
+          result: { runId: content.finalResult.runId },
+          args: {
+            __mastraMetadata: {
+              from: 'AGENT',
+              messages: badgeMessages,
+            },
+          },
+        },
+      ],
+    };
   }
 
   return { role: 'assistant', content: [{ type: 'text', text: 'blah' }] };
