7675174ea072b7046924d55beaceef2839cc4604
mfrachet <marvin@mastra.ai>
2025-09-10 15:17:46 +0200

    first network ui

--- packages/playground-ui/src/services/handle-network-message-from-memory.ts
+++ packages/playground-ui/src/services/handle-network-message-from-memory.ts
@@ -1,16 +1,23 @@
 import { ThreadMessageLike } from '@assistant-ui/react';
 
 export const handleNetworkMessageFromMemory = (content: any): ThreadMessageLike => {
-  console.log('content', content);
-  return {
-    role: 'assistant',
-    content: [
-      {
-        type: 'tool-call',
-        toolCallId: content.finalResult.runId,
-        toolName: content.resourceId,
-        result: { runId: content.finalResult.runId },
-      },
-    ],
-  };
+  if (content.resourceType === 'workflow') {
+    return {
+      role: 'assistant',
+      content: [
+        {
+          type: 'tool-call',
+          toolCallId: content.finalResult.runId,
+          toolName: content.resourceId,
+          result: { runId: content.finalResult.runId },
+        },
+      ],
+    };
+  }
+
+  if (content.resourceType === 'agent') {
+    console.log('content', content);
+  }
+
+  return { role: 'assistant', content: [{ type: 'text', text: 'blah' }] };
 };
